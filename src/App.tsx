2bb274c385db6f61f6c82d7c1125528c66171534
Dante Benicio <dantecosta79@gmail.com>
2022-04-19 12:23:33 -0300

    app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import './App.css'
+import { useState } from 'react';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+
+export default function App() {
+  const [openBurger, setOpenBurger] = useState<boolean>(false);
+
+  const toggleBurgerMenu = () => setOpenBurger(prevState => !prevState);
 
 function App() {
   return (
